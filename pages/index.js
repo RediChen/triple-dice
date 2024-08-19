9ca173f52a694b97680729f6ff272c21b5cadff3
RediChen <a0929052958@gmail.com>
2024-08-19 21:08:59 +0800

    新增按鈕數量的調整功能以及極端情形處遇

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 // import Image from 'next/image'
-import { Noto_Sans_TC } from 'next/font/google'
+import { Noto_Sans_TC, Yuji_Mai } from 'next/font/google'
 import s from '@/styles/main.module.scss'
 import StarStr from '@/components/StarStr'
 import { useEffect, useState } from 'react'
@@ -8,22 +8,71 @@ import { useEffect, useState } from 'react'
 import Card from '@/components/Card'
 
 const noto = Noto_Sans_TC({ subsets: ['latin'] })
+const yuji = Yuji_Mai({ subsets: ['latin'], weight: '400' })
+
+/**
+ * 卡片的數量上限
+ */
+const NUM_SLOT = 8
+const initCard = Array(NUM_SLOT).fill(0)
+const initSlot = Array(NUM_SLOT).fill(false)
 
 export default function Home() {
-  const THREE = [0, 0, 0]
-  const THREE_F = [false, false, false]
+  /** 卡片的啟動數量 */
+  const [numCard, setNumCard] = useState(3)
+  const initStateArr = initSlot.map((_, j) => j < numCard)
+  const [cardState, setCardState] = useState(initStateArr)
+
+  // 背景的播放狀態
   const [isPaused, setIsPaused] = useState(false)
-  const [resultArr, setResultArr] = useState(THREE)
-  const [cardShakeState, setCardShakeState] = useState(THREE_F)
-  const [cardFlipState, setCardFlipState] = useState(THREE_F)
+  // 骰得的結果數列
+  const [resultArr, setResultArr] = useState(initCard)
+  // 卡片的動態狀態：搖搖
+  const [cardShakeState, setCardShakeState] = useState(initSlot)
+  // 卡片的動態狀態：翻面
+  const [cardFlipState, setCardFlipState] = useState(initSlot)
+  // special message
+  // 0 - 減過頭的情形
+  // 1 - 加過頭的情形
+  const initMsgState = [false, false]
+  const [msgState, setMsgState] = useState(initMsgState)
 
   const numList = [2, 3, 5, 7, 10, 11]
 
-  const waitingFor = (t) => {
-    new Promise((resolve, reject) => {
-      setTimeout(resolve, t)
-    })
-  }
+  useEffect(() => {
+    const newArr = initSlot.map((_, j) => j < numCard)
+    setCardState(newArr)
+  }, [numCard])
+
+  useEffect(() => {
+    if (resultArr.every((num) => num === 0)) return
+
+    setCardShakeState(initCard.map((b) => !b))
+    const timerID = setTimeout(() => {
+      setCardFlipState(initCard.map((b) => !b))
+      setCardShakeState(initSlot)
+    }, 1000)
+
+    return () => clearTimeout(timerID)
+    //eslint-disable-next-line
+  }, [resultArr])
+
+  useEffect(() => {
+    if (msgState.every((num) => num === false)) return
+
+    const timerID = setTimeout(() => {
+      setMsgState(initMsgState)
+    }, 2000)
+
+    return () => clearTimeout(timerID)
+    //eslint-disable-next-line
+  }, [msgState])
+
+  // const waitingFor = (t) => {
+  //   new Promise((resolve, reject) => {
+  //     setTimeout(resolve, t)
+  //   })
+  // }
 
   const tossDice = (list) => {
     const n = numList.length
@@ -31,15 +80,15 @@ export default function Home() {
     return list[i]
   }
 
-  const resetIfNeed = () => {
-    if (cardFlipState.some((v) => v)) {
-      setCardFlipState(THREE_F)
-    }
-  }
+  // const resetIfNeed = () => {
+  //   if (cardFlipState.some((v) => v)) {
+  //     setCardFlipState(initCard_F)
+  //   }
+  // }
 
   const handleDice = async () => {
     if (cardFlipState.some((v) => v)) {
-      setCardFlipState(THREE_F)
+      setCardFlipState(initSlot)
       setTimeout(() => {
         setResultArr(resultArr.map((_) => tossDice(numList)))
       }, 2000)
@@ -47,24 +96,26 @@ export default function Home() {
       setResultArr(resultArr.map((_) => tossDice(numList)))
     }
   }
-  useEffect(() => {
-    if (resultArr.every((num) => num === 0)) return
 
-    setCardShakeState(THREE.map((b) => !b))
-    const timerID = setTimeout(() => {
-      setCardFlipState(THREE.map((b) => !b))
-      setCardShakeState(THREE_F)
-    }, 1000)
+  const handleCardNumber = (delta) => {
+    if (delta < 0) {
+      numCard === 1
+        ? setMsgState(msgState.map((_, i) => i === 0))
+        : setNumCard(numCard - 1)
+    } else {
+      numCard === NUM_SLOT
+        ? setMsgState(msgState.map((_, i) => i === 1))
+        : setNumCard(numCard + 1)
+    }
+  }
 
-    return () => clearTimeout(timerID)
-    //eslint-disable-next-line
-  }, [resultArr])
   return (
     <>
       <Head>
         <title>骰子卡比</title>
         <meta name="viewport" content="viewport-fit=cover, initial-scale=1" />
       </Head>
+      {/*===========================//* 背景 */}
       <div className={s.bg}>
         <div className={[s.bg_row, isPaused ? s.pause : ''].join(' ')}>
           <StarStr />
@@ -79,6 +130,7 @@ export default function Home() {
           <StarStr />
         </div>
       </div>
+      {/*===========================//* 介面 */}
       <main className={[s.main, noto.className].join(' ')}>
         <button
           className={['btn btn-outline-warning', s.pauseBtn].join(' ')}
@@ -87,26 +139,57 @@ export default function Home() {
           暫停動畫
         </button>
         <button
+          className={['btn btn-warning', s.pauseBtn].join(' ')}
+          onClick={() => handleCardNumber(-1)}
+        >
+          減
+        </button>
+        <button
+          className={['btn btn-warning', s.pauseBtn].join(' ')}
+          onClick={() => handleCardNumber(1)}
+        >
+          加
+        </button>
+        <button
           className={['btn btn-success', s.diceBtn].join(' ')}
           onClick={() => handleDice()}
         >
           骰
         </button>
-        <div></div>
+        <div>這個要作常駐選單按鈕</div>
       </main>
+      {/*===========================//* 卡片 */}
       <div className={s.panel}>
         <div className="row row-cols-1 row-cols-lg-3 gap-5 justify-content-around align-items-center">
-          {THREE.map((_, i) => (
-            <Card
-              key={i}
-              doesFlip={cardFlipState[i]}
-              doesShake={cardShakeState[i]}
-            >
-              {resultArr[i]}
-            </Card>
-          ))}
+          {cardState.map(
+            (isActive, i) =>
+              isActive && (
+                <Card
+                  key={i}
+                  doesFlip={cardFlipState[i]}
+                  doesShake={cardShakeState[i]}
+                >
+                  {resultArr[i]}
+                </Card>
+              )
+          )}
         </div>
       </div>
+      {/*===========================//* 訊息 */}
+      {msgState[0] && (
+        <article className={s.message}>
+          <p className={['text-danger', yuji.className].join(' ')}>
+            誰把老師惹毛了！
+          </p>
+        </article>
+      )}
+      {msgState[1] && (
+        <article className={s.message}>
+          <p className={['text-danger', yuji.className].join(' ')}>
+            做人不要太貪心！
+          </p>
+        </article>
+      )}
     </>
   )
 }
